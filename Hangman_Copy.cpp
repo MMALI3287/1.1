fe5b4e13183450a63f5ffdbca2a7b35b7195696e
MD Musaddique Ali Erfan <erfanali3287@gmail.com>
2022-01-20 03:14:07 +0600

    beforegapproblem

--- Hangman_Copy.cpp
+++ Hangman_Copy.cpp
@@ -1,3 +1,4 @@
+//***********************************************************Header Files*********************************************************************//
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -6,28 +7,27 @@
 #include <iostream>
 #include <random>
 #include <windows.h>
-#define MAX_STRING_SIZE 1000
+#define MAX_STRING_SIZE 5000
 using namespace std;
+//***********************************************************Functions*********************************************************************//
 int randNum()
 {
     mt19937 mt(time(nullptr));
-    unsigned int a = mt() % 4 + 1;
+    unsigned int a = mt() % 100 + 1;
     return a;
 }
-void sleep(unsigned milliseconds)
-{
-    Sleep(milliseconds);
-}
+//*************************************************************Main*********************************************************************//
 int main()
 {
-    int numOfBlank = 0, j = 0, gap = 0, curRanNum = 0, prevRanNum = 0, dash[2], b;
+    // Variables//
+    int numOfBlank = 0, dashInd = 0, gap = 0, curRanNum = 0, prevRanNum = 0, dash[2];
     char letter;
     char str[MAX_STRING_SIZE], pick[MAX_STRING_SIZE];
     FILE *fp;
     int readCount = 0;
 
+    // Pick a Random Word from txt File//
     srand(time(0));
-
     fp = fopen("Hangman.txt", "r");
     if (fp)
     {
@@ -36,6 +36,7 @@ int main()
             readCount = 1;
             while (fgets(str, MAX_STRING_SIZE, fp) != NULL)
             {
+
                 if ((rand() % ++readCount) == 0)
                 {
                     strcpy(pick, str);
@@ -45,14 +46,16 @@ int main()
         }
     }
     fclose(fp);
-    string word = pick;
-    char word2[word.length() - 1];
+    // declaring word1 & word2//
+    string word1 = pick;
+    char word2[word1.length() - 1];
+    // entering '_' in random index in word2//
     while (gap < 2)
     {
-        while (curRanNum == prevRanNum)
+        while (curRanNum <= prevRanNum)
         {
-            curRanNum = randNum();
-            // cout << curRanNum << endl;
+            curRanNum = randNum() % 5 + 1;
+            cout << curRanNum << endl;
         }
         if (curRanNum != prevRanNum)
         {
@@ -61,25 +64,29 @@ int main()
         }
         prevRanNum = curRanNum;
         dash[gap] = curRanNum;
+        // Sleep(2000);
         gap++;
     }
-    for (int i = 0; i <= word.length() - 1; i++)
+    // filling other index of word2 which does not include '_'//
+    for (int i = 0; i <= word1.length() - 1; i++)
     {
         if (word2[i] != '_')
-            word2[i] = word[i];
+            word2[i] = word1[i];
     }
-    for (int i = 0; i <= word.length() - 1; i++)
+    // printing word2//
+    for (int i = 0; i <= word1.length() - 1; i++)
     {
         cout << word2[i] << " ";
     }
-    while (j < numOfBlank)
+    // taking letter input which is missing in word2//
+    while (dashInd < numOfBlank)
     {
         cout << endl;
         cin >> letter;
-        if (letter == word[dash[j]])
+        if (letter == word1[dash[dashInd]])
         {
-            word2[dash[j]] = letter;
-            for (int i = 0; i <= word.length() - 1; i++)
+            word2[dash[dashInd]] = letter;
+            for (int i = 0; i <= word1.length() - 1; i++)
             {
                 cout << word2[i] << " ";
             }
@@ -87,10 +94,11 @@ int main()
         else
         {
             cout << "Your Ans is Wrong" << endl;
-            cout << "Right Ans: " << word << endl;
+            cout << "Right Ans: " << word1 << endl;
             break;
         }
-        j++;
+        dashInd++;
     }
     return 0;
 }
+//**************************************************************End*********************************************************************//
