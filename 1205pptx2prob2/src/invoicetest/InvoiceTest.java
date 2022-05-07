69855fbe3f752d9addbb8a8a5a136d93a5c42626
MD Musaddique Ali Erfan <erfanali3287@gmail.com>
2022-05-08 03:39:40 +0600

    8.5.22 v1

--- 1205pptx2prob2/src/invoicetest/InvoiceTest.java
+++
@@ -1,7 +0,0 @@
-package invoicetest;
-public class InvoiceTest {
-    public static void main(String[] args) {
-        Invoice i1=new Invoice("001","Special Delivary",12,100000);
-        System.out.println("Total Invoice Amount: "+i1.getInvoiceAmount());
-    }    
-}
